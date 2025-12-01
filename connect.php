85f377f4aba6cab4426134678b6aa7a5d303cd93
khalifa5000 <m.khalifa5000@gmail.com>
2025-12-01 22:31:10 +0200

    first commit

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+
+// Database connection parameters
+$host = 'localhost';
+$db   = 'simple_project';
+$user = 'root';
+$pass = '';  
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Connected successfully";
+} catch (PDOException $e) { 
+    echo "Connection failed: " . $e->getMessage();
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+  
+}
+?>
