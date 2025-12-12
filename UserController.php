8e604babf4fc737e64c0c758b6aaafc51bbb9725
khalifa5000 <m.khalifa5000@gmail.com>
2025-12-12 20:05:26 +0200

    Edit add file

--- UserController.php
+++ UserController.php
@@ -1,6 +1,6 @@
 <?php
 
- 
+    include 'connect.php';
 
     $name = $_POST['name']??'';
     $email = $_POST['email']??'';
@@ -10,16 +10,24 @@
     echo "<br>" . "Received Name: $name" . "<br>" . " Email: $email";
 
 
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE E_mail = ?');
+    $stmt->execute([$email]);
+ 
+    $count = $stmt->rowCount();
+
+    echo $count;
+
+
 
     if ($name && $email) {
-        include 'connect.php';
+ 
 
         $stmt = $pdo->prepare('INSERT INTO users (Name, E_mail) VALUES (?, ?)');
         $stmt->execute([$name, $email]);
 
         echo "<br>" . "New user added successfully.";
     } else {
-        echo "Name and Email are required.";
+        echo "<br>" . "Name and Email are required." . $e->getMessage();
     }
 
 ?>
